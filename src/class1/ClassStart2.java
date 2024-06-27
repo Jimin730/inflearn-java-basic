95c24fe5db17025add5b1c3025ac5f3fe31af9b5
Jimin730 <dnddnd120@naver.com>
2024-06-27 16:23:14 +0900

    java-basic git 연동

--- src/class1/ClassStart2.java
+++ src/class1/ClassStart2.java
@@ -10,6 +10,7 @@ public class ClassStart2 {
         int[] studentAges = {15, 16, 17, 20};
         int[] studentGrades = {90, 80, 70, 60};
 
+
         for (int i = 0; i < studentNames.length; i++){
             System.out.println("이름:" + studentNames[i] + " 나이:" + studentAges[i] + " 성적:" + studentGrades[i]);
         }
